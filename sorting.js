36c4787179676f621e0058b3ad0a14db2815fa48
MarkWaldron <markewaldron@gmail.com>
2015-11-05 18:06:20 -0500

    Add passing functions for bubble and merge sort

--- sorting.js
+++ sorting.js
@@ -3,7 +3,7 @@ function bubbleSort (arr) {
         for (var j = 0; j < arr.length; j++) {
                 var x = arr[i],
                     y = arr[i + 1];
-                if ( x > y) {
+                if (x > y) {
                     arr[i + 1] = x;
                     arr[i] = y;
                 }
@@ -11,3 +11,52 @@ function bubbleSort (arr) {
     };
     return arr;
 }
+
+function split(wholeArray) {
+  var firstHalf, secondHalf;
+  if(wholeArray.length < 2) return;
+  if(wholeArray.length % 2 !== 0){
+    var firstHalf = wholeArray.slice(0, Math.floor(wholeArray.length/2));
+    var secondHalf = wholeArray.slice(firstHalf.length, wholeArray.length);
+  } else {
+    var firstHalf = wholeArray.slice(0, wholeArray.length/2);
+    var secondHalf = wholeArray.slice(firstHalf.length, wholeArray.length);
+  }
+
+  return [firstHalf, secondHalf];
+}
+
+function merge(arrOne, arrTwo){
+  var result = [];
+
+  while (arrOne.length && arrTwo.length) {
+    if(arrOne[0] < arrTwo[0]){
+      result.push(arrOne.shift());
+    } else if (arrTwo[0] < arrOne[0]){
+      result.push(arrTwo.shift());
+    }
+  }
+  if(arrOne.length){
+    result = result.concat(arrOne);
+  } else if (arrTwo.length) {
+    result = result.concat(arrTwo);
+  }
+  return result;
+};
+
+
+function mergeSort(arr){
+  var result = [];
+  var x, y;
+  if(arr.length == 1){
+    return arr;
+  } else {
+    var splitArrays = split(arr);
+
+    x = mergeSort(splitArrays[0]);
+    console.log("x: " + x);
+    y = mergeSort(splitArrays[1]);
+    console.log("y: " + y);
+  }
+  return merge(x, y);
+};
--- sortingSpec.js
+++ sortingSpec.js
@@ -1,5 +1,24 @@
 describe('Bubble Sort', function(){
     it('handles an empty array', function(){
-        expect( bubbleSort([2,1,4,8,3]) ).toEqual( [1,2,3,4,8] );
+        expect(bubbleSort([2,1,4,8,3])).toEqual([1,2,3,4,8]);
+    });
+});
+
+describe('Split Function', function(){
+  it('is able to split one array into two', function(){
+    expect(split([1, 2, 3, 4, 5, 6])).toEqual([[1, 2, 3],[4, 5, 6]]);
+  });
+});
+
+
+describe('Merge', function(){
+    it('is able to merge two sorted arrays', function(){
+        expect(merge([1, 2, 3], [4, 5, 6])).toEqual([1,2,3,4,5,6]);
+    });
+});
+
+describe('Merge Sort', function() {
+    it('takes an unsorted array, sorts it gooddd', function(){
+        expect(mergeSort([5, 4, 3, 2, 1, 10, 9, 8, 7, 6])).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     });
 });
